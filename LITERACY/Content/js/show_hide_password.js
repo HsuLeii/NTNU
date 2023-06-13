a1e28b0fbe45f4a231608cd5289e2258f21bd5cb
HsuLeii <ok841113@gmail.com>
2023-06-13 17:29:43 +0800

    0613

--- LITERACY/Content/js/show_hide_password.js
+++ LITERACY/Content/js/show_hide_password.js
@@ -1,14 +1,12 @@
-//密碼顯示/隱藏
+	//密碼顯示/隱藏
 	$(".icon_eye").on('click', function(event) {
 		if( $(this).siblings('.input_password').val() != "" ){
 			if( $(this).siblings('.input_password').attr("type") == "text"){//原本是顯示密碼
 				$(this).siblings('.input_password').attr('type', 'password');//變成隱藏密碼
-				$(this).find('i').addClass( "fa-eye-slash" );
-				$(this).find('i').removeClass( "fa-eye" );
+				$(this).find('svg').attr('data-icon', 'eye-slash');
 			}else if($('.input_password').attr("type") == "password"){//原本是隱藏密碼
 				$(this).siblings('input').attr('type', 'text');//變成顯示密碼
-				$(this).find('i').removeClass( "fa-eye-slash" );
-				$(this).find('i').addClass( "fa-eye" );
+				$(this).find('svg').attr('data-icon', 'eye');
 			}
 		}
 	});
